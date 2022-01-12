ecbc7b1a5517e2008576d0d413e56716c0ae5758
beruf <rube@ruf2.de>
2022-01-12 10:22:16 +0100

    deployments

---
+++ deploy/aydea/deploy_allowlist_draggable_aydea.js
@@ -0,0 +1,57 @@
+const Confirm = require('prompt-confirm');
+
+module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
+  const { deploy } = deployments;
+
+  const { deployer } = await getNamedAccounts();
+
+  const shares = await deployments.get('AllowlistSharesAydea');
+  const recoveryHub = await deployments.get("RecoveryHub");
+  const offerFactory = await deployments.get("OfferFactory");
+
+  // owner of allowlistshares and allowlist draggableshares is the same.
+  // no need to create another multisig wallet
+  const sharesContract = await ethers.getContractAt("AllowlistShares", shares.address);
+  const owner = await sharesContract.owner();
+  
+  const terms = "wwww.terms.ch";
+  const quorumBps = 75000;
+  const votePeriodSeconds = 5184000;
+
+  if (network.name != "hardhat") {
+    console.log("-----------------------")
+    console.log("Deploy Allowlist DraggableShares")
+    console.log("-----------------------")
+    console.log("deployer: %s", deployer);
+    console.log("owner: %s", owner)  // don't forget to set it in hardhat.config.js as the multsig account
+    
+    const prompt = await new Confirm("Addresses correct?").run();
+    if(!prompt) {
+      console.log("exiting");
+      process.exit();
+    }
+  }
+  
+  const feeData = await ethers.provider.getFeeData();
+
+  const { address } = await deploy("AllowlistDraggableSharesAydea", {
+    contract: "AllowlistDraggableShares",
+    from: deployer,
+    args: [
+      terms,
+      shares.address,
+      quorumBps,
+      votePeriodSeconds,
+      recoveryHub.address,
+      offerFactory.address,
+      owner,
+      owner],
+    log: true,
+    maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
+    maxFeePerGas: feeData.maxFeePerGas,
+    gasLimit: 3000000
+  });
+};
+
+module.exports.tags = ["AllowlistDraggableSharesAydea"];
+module.exports.dependencies = ["RecoveryHub", "OfferFactory", "AllowlistSharesAydea"];
---
+++ deploy/aydea/deploy_allowlist_shares_aydea.js
@@ -0,0 +1,48 @@
+const Confirm = require('prompt-confirm');
+
+module.exports = async function ({ ethers, deployments, getNamedAccounts }) {
+  const { deploy } = deployments;
+
+  const { deployer, owner } = await getNamedAccounts();
+
+  const recoveryHub = await deployments.get("RecoveryHub");
+  
+  const symbol = "AYS";
+  const name = "TAydea Shares";
+  const terms = "https://aydea.ch";
+  const totalShares = 35000;
+
+  if (network.name != "hardhat") {
+    console.log("-----------------------")
+    console.log("Deploy Allowlist Shares")
+    console.log("-----------------------")
+    console.log("deployer: %s", deployer);
+    console.log("owner: %s", owner)  // don't forget to set it in hardhat.config.js as the multsig account
+    
+    const prompt = await new Confirm("Addresses correct?").run();
+    if(!prompt) {
+      console.log("exiting");
+      process.exit();
+    }
+  }
+  
+  const feeData = await ethers.provider.getFeeData();
+
+  const { address } = await deploy("AllowlistSharesAydea", {
+    contract: "AllowlistShares",
+    from: deployer,
+    args: [
+      symbol,
+      name,
+      terms,
+      totalShares,
+      recoveryHub.address,
+      owner],
+    log: true,
+    maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
+    maxFeePerGas: feeData.maxFeePerGas
+  });
+};
+
+module.exports.tags = ["AllowlistSharesAydea"];
+module.exports.dependencies = ["RecoveryHub"];
